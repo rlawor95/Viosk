3f0e0bfd634a85ff07b4ff28433c66ff29e506c8
KimJack <kjk71978292@gmail.com>
2021-12-18 21:38:20 +0900

    page2 작업중

---
+++ Assets/Script/CustomBtn.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomBtn : MonoBehaviour
+{
+    public int ID;
+    public bool IsSelected = false;
+
+    public Sprite NormalImage;
+    public Sprite SelectionImage;
+
+    Image _img;
+
+    void Start()
+    {
+        EventMgr.Instance.ClickEvent += ReceiveClickEvent;
+
+        _img = GetComponent<Image>();
+        SetSprite();
+        
+    }
+
+    public void BtnClickEvent()
+    {
+        if(IsSelected)
+        {
+            IsSelected =false;
+            SetSprite();
+        }
+        else
+        {
+             EventMgr.Instance.InvokeClickEvent(ID);
+             IsSelected = true;
+             SetSprite();
+        }
+       
+    }
+
+    //어떤 버튼에 클릭이 들어오면 모든 버 
+    void ReceiveClickEvent(int id)
+    {
+        if (id != this.ID)
+            return;
+
+        IsSelected = false;
+        SetSprite();
+    }
+
+    void SetSprite()
+    {
+        if (IsSelected)
+            _img.sprite = SelectionImage;
+        else
+            _img.sprite = NormalImage;
+    }
+}
---
+++ Assets/Script/EventMgr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventMgr : MonoBehaviour
+{
+    public static EventMgr Instance = null;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    public delegate void ClickDelegate(int id);
+    public ClickDelegate ClickEvent;
+    
+    public void InvokeClickEvent(int id)
+    {
+        ClickEvent?.Invoke(id);
+    }
+
+}
