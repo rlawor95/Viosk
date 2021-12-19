49e75df41c4eb24f139f860a64f9051f311d5157
KimJack <kjk71978292@gmail.com>
2021-12-19 20:42:34 +0900

    리소스 작업시작 전

---
+++ Assets/Script/AddMenuMgr.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddMenuMgr : MonoBehaviour
+{
+    public GameObject GradiantContent;
+    public GameObject FryContent;
+    public GameObject DrinkContent;
+    public GameObject SauceContent;
+
+
+    public Text TotalPriceTxt;
+
+    void Start()
+    {
+
+    }
+
+    
+}
---
+++ Assets/Script/AddMenuObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddMenuObject : MonoBehaviour
+{
+    public Image Icon;
+    public Text infoTxt;
+
+
+    public int Price = 0;
+
+    public void SetInfo(string _infoTxt, int price, Sprite _icon = null)
+    {
+        infoTxt.text = _infoTxt;
+        if (_icon != null)
+            Icon.sprite = _icon;
+
+        Price = price;
+    }
+}
